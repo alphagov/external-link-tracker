52a5d42904eac1e52b00f86adbd8894f252089fd
Bradley Wright <bradley.wright@digital.cabinet-office.gov.uk>
2013-12-06 16:08:28 +0000

    Look up external URL in Mongo before redirecting

--- main.go
+++ main.go
@@ -1,16 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 	"net/http"
 )
 
-func externalLinkHandler(w http.ResponseWrite, r *http.Request) {
-	external_url := r.URL.Query().Get("url")
-	// Explicit 302 because this is a redirection proxy
-	http.Redirect(w, r, external_url, http.StatusFound)
+type ExternalLink struct {
+	ExternalUrl string `bson:"external_url"`
+	HitCount    int32  `bson:"hit_count"`
+}
+
+func externalLinkTrackerHandler(mongoUrl string, mongoDbName string) func(http.ResponseWriter, *http.Request) {
+
+	return func (w http.ResponseWriter, req *http.Request) {
+		sess, err := mgo.Dial(mongoUrl)
+		if err != nil {
+			panic(fmt.Sprintln("mgo:", err))
+		}
+		defer sess.Close()
+		sess.SetMode(mgo.Monotonic, true)
+
+		db := sess.DB(mongoDbName)
+		collection := db.C("links")
+
+
+		external_url := req.URL.Query().Get("url")
+		println(external_url)
+		result := ExternalLink{}
+		err1 := collection.Find(bson.M{"external_url": external_url}).One(&result)
+
+		if err1 != nil {
+			panic(err1)
+		}
+		println("Found:", result.ExternalUrl)
+		// Explicit 302 because this is a redirection proxy
+		http.Redirect(w, req, external_url, http.StatusFound)
+	}
 }
 
 func main() {
-	http.HandleFunc("/g", externalLinkHandler)
+	http.HandleFunc("/g", externalLinkTrackerHandler("localhost", "external_link_tracker"))
 	http.ListenAndServe("127.0.0.1:8080", nil)
+
 }
