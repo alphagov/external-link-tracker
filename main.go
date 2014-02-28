11c40ccaf50e6e7270661a27ddc4e55cc04ea0d3
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-02-28 12:53:01 +0000

    Server will resume listening on file descriptors
    If a pair of file descriptors are passed in the LISTEN_FD and
    API_LISTEN_FD env variables, the server will resume listening on those
    instead of opening a new listen socket.

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"sync"
 
+	"github.com/alext/graceful_listener"
 	"github.com/codegangsta/martini"
 )
 
@@ -22,25 +25,57 @@ func getenvDefault(key string, defaultVal string) string {
 	return val
 }
 
-func catchListenAndServe(addr string, handler http.Handler) {
-	err := http.ListenAndServe(addr, handler)
+func main() {
+	listener, apiListener, err := createListeners()
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func main() {
 	m := martini.Classic()
 	m.Get("/g", ExternalLinkTrackerHandler)
 	mApi := martini.Classic()
 	mApi.Put("/url", AddExternalURL)
 
-	go catchListenAndServe(pubAddr, m)
-	log.Println("external-link-tracker: listening for redirects on " + pubAddr)
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	go catchListenAndServe(apiAddr, mApi)
-	log.Println("external-link-tracker: listening for writes on " + apiAddr)
+	go serve(listener, m, &wg)
+	log.Println("external-link-tracker: listening for redirects on " + listener.Addr().String())
 
-	dontQuit := make(chan int)
-	<-dontQuit
+	go serve(apiListener, mApi, &wg)
+	log.Println("external-link-tracker: listening for writes on " + apiListener.Addr().String())
+
+	wg.Wait()
+}
+
+func createListeners() (listener, apiListener *graceful_listener.Listener, err error) {
+	listenFD, err := strconv.Atoi(getenvDefault("LISTEN_FD", "0"))
+	if err != nil {
+		log.Println("Non-integer LISTEN_FD, ignoring:", err)
+		listenFD = 0
+	}
+	apiListenFD, err := strconv.Atoi(getenvDefault("API_LISTEN_FD", "0"))
+	if err != nil {
+		log.Println("Non-integer API_LISTEN_FD, ignoring:", err)
+		apiListenFD = 0
+	}
+
+	listener, err = graceful_listener.ResumeOrStart(listenFD, pubAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+	apiListener, err = graceful_listener.ResumeOrStart(apiListenFD, apiAddr)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return
+}
+
+func serve(l *graceful_listener.Listener, handler http.Handler, wg *sync.WaitGroup) {
+	err := http.Serve(l, handler)
+	if err != nil {
+		log.Fatal("serve error: ", err)
+	}
+	wg.Done()
 }
